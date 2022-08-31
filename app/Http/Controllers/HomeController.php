deaa4c7f292fe7e3da8a8e438233ac23d3cda270
Leon McCarthy <leon@pop-os.localdomain>
2022-08-31 10:37:23 +0300

    added displaying food from database to frontend

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,21 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Food;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home');
+        $data = Food::all();
+        return view('home', compact("data"));
     }
 
     public function redirects(){
         // checking for the usertype from database (admin or normal user)
         $usertype = Auth::user()->usertype;
+        $data = Food::all();
 
         if($usertype=='1'){
             return view('admin.admin-home');
         }else{
-            return view('home');
+            return view('home', compact("data"));
         }
     }
 }
